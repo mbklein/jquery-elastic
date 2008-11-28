4587050e4ae9c5e9f42e05801cf992a589946651
jan.jarfalk <jan.jarfalk@14aa3af0-b55e-11dd-a7db-b583d5cdf5c2>
2008-11-28 16:29:49 +0000

    Adding version 1
    git-svn-id: http://jquery-elastic.googlecode.com/svn/trunk@2 14aa3af0-b55e-11dd-a7db-b583d5cdf5c2

---
+++ jquery.elastic.source.js
@@ -0,0 +1,41 @@
+(function($){ 
+     $.fn.extend({  
+         elastic: function() { 
+             
+			var mimics = new Array('paddingTop','paddingRight','paddingBottom','paddingLeft','fontSize','lineHeight','fontFamily','width');	
+            return this.each(function() { 
+         		
+				if(this.type == 'textarea') {
+				
+					var textarea = $(this);
+					var minheight = parseInt(textarea.css('height'));
+					var marginbottom = parseInt(textarea.css('lineHeight'))*2 || parseInt(textarea.css('fontSize'))*2;
+					var goalheight = 0;
+					var twin = null;
+					
+					function update() {
+						if (!twin)
+						{
+							twin = $('<div />').css({'visibility': 'hidden','position': 'absolute','overflow-x': 'hidden'}).appendTo('body');
+							$.each(mimics, function(){
+								twin.css(this,textarea.css(this));
+							});
+						}
+						
+
+						var content = textarea.val().replace(/<|>/g, ' ').replace(/\n/g, '<br />');
+						if (twin.text() != content)
+						{				
+							twin.html(content);
+							goalheight = (twin.height()+marginbottom > minheight)?twin.height()+marginbottom:minheight;
+							if(goalheight != textarea.height())
+								textarea.animate({'height': goalheight},500);		
+						}
+					}
+					textarea.css({overflow: 'hidden', display: 'block'}).bind('focus', function() { self.periodicalUpdater = window.setInterval(function() {update()}, 400); }).bind('blur', function() { clearInterval(self.periodicalUpdater); });
+					update();
+				}
+            }); 
+        } 
+    }); 
+})(jQuery);
