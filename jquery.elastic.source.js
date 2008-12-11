3632da00c6a4451200e289a28ac846905466b3db
jan.jarfalk <jan.jarfalk@14aa3af0-b55e-11dd-a7db-b583d5cdf5c2>
2008-12-11 07:35:42 +0000

    Ficed bug in IE6
    git-svn-id: http://jquery-elastic.googlecode.com/svn/trunk@4 14aa3af0-b55e-11dd-a7db-b583d5cdf5c2

--- jquery.elastic.source.js
+++ jquery.elastic.source.js
@@ -1,15 +1,14 @@
 (function($){ 
      $.fn.extend({  
          elastic: function() { 
-             
 			var mimics = new Array('paddingTop','paddingRight','paddingBottom','paddingLeft','fontSize','lineHeight','fontFamily','width');	
-            return this.each(function() { 
-         		
+			return this.each(function() { 
+         			
 				if(this.type == 'textarea') {
-				
+					
 					var textarea = $(this);
-					var minheight = parseInt(textarea.css('height'));
 					var marginbottom = parseInt(textarea.css('lineHeight'))*2 || parseInt(textarea.css('fontSize'))*2;
+					var minheight = parseInt(textarea.css('height')) || marginbottom;
 					var goalheight = 0;
 					var twin = null;
 					
@@ -24,15 +23,16 @@
 						
 						var content = textarea.val().replace(/<|>/g, ' ').replace(/\n/g, '<br />');
 						if (twin.text() != content)
-						{				
+						{			
 							twin.html(content);
 							goalheight = (twin.height()+marginbottom > minheight)?twin.height()+marginbottom:minheight;
 							if(goalheight != textarea.height())
 								textarea.animate({'height': goalheight},500);		
 						}
 					}
-					textarea.css({overflow: 'hidden', display: 'block'}).bind('focus', function() { self.periodicalUpdater = window.setInterval(function() {update()}, 400); }).bind('blur', function() { clearInterval(self.periodicalUpdater); });
+					textarea.css({overflow: 'hidden',display: 'block'}).bind('focus',function() { self.periodicalUpdater = window.setInterval(function() {update();}, 400); }).bind('blur', function() { clearInterval(self.periodicalUpdater); });
 					update();
+					
 				}
             }); 
         } 
